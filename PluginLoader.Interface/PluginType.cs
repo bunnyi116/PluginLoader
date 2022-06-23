5622250fca32d7fdbbabdbfeebf7782237f010c1
JianJie Huang <1160706050@qq.com>
2022-06-23 12:48:48 +0800

    添加注释

--- PluginLoader.Interface/PluginType.cs
+++ PluginLoader.Interface/PluginType.cs
@@ -5,9 +5,24 @@
     /// </summary>
     public enum PluginType
     {
+        /// <summary>
+        /// 默认的
+        /// </summary>
         Default = 0,
+
+        /// <summary>
+        /// 带插件加载接口
+        /// </summary>
         NeedPluginLoader = 1,
+
+        /// <summary>
+        /// 带命令
+        /// </summary>
         NeedCommands = 2,
+
+        /// <summary>
+        /// 带依赖
+        /// </summary>
         NeedDepend = 4,
     }
 }
